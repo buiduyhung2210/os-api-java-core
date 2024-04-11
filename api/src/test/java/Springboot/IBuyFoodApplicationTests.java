6325ebf0368ec432fafc94493499c9f9bf7841a0
buiduyhung2210 <buiduyhung2210@gmail.com>
2024-04-11 14:38:01 +0700

    first innit

---
+++ api/src/test/java/Springboot/IBuyFoodApplicationTests.java
@@ -0,0 +1,13 @@
+package Springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IBuyFoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
